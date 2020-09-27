835cd3a6c6929d8fb63541e2bbdafb470b75d5ae
toriumi <ken.tennis914@gmail.com>
2020-09-28 00:26:35 +0900

    update 1.0.x

--- constantsEx/env.js
+++ constantsEx/env.js
@@ -1,6 +1,11 @@
-const BASE_HOST = "192.168.11.2:8080";
+// const BASE_HOST = "192.168.11.2:8080";
+// const URL_SCHEME_HTTP = "http";
+// const WS_SCHEME_HTTP = "ws";
+const BASE_HOST = "fullfii.com";
+const URL_SCHEME_HTTP = "https";
+const WS_SCHEME_HTTP = "wss";
 
-export const BASE_URL = `http://${BASE_HOST}/api/v1/`;
-export const BASE_URL_WS = `ws://${BASE_HOST}/ws/`;
-export const USER_POLICY_URL = "https://www.fullfii.com/pages/privacy";
+export const BASE_URL = `${URL_SCHEME_HTTP}://${BASE_HOST}/api/v1/`;
+export const BASE_URL_WS = `${WS_SCHEME_HTTP}://${BASE_HOST}/ws/`;
+export const USER_POLICY_URL = "https://fullfii.com/terms-of-service/";
 export const GOOGLE_FORM_URL = "https://docs.google.com/forms/d/e/1FAIpQLScaGHQYXpvYtPPSIKqVgPdSgM5QY_dzOQeTG6j8Jz16bJWV3A/viewform?usp=sf_link";
