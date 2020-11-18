473f6f042744c1380cf8bddd0ab7ab25ec96340d
kensan914 <52157596+kensan914@users.noreply.github.com>
2020-11-19 00:37:23 +0900

    警告非表示

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 import { registerRootComponent } from "expo";
 import App from "./App";
-import { LogBox } from "react-native";
 
-LogBox.ignoreAllLogs(true);
 
 // registerRootComponent calls AppRegistry.registerComponent("main", () => App);
 // It also ensures that whether you load the app in the Expo client or in a native build,
