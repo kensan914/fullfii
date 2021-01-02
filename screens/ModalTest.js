c73a883a0b4b62ab4182a0440fc487ace412c80d
kensan914 <52157596+kensan914@users.noreply.github.com>
2021-01-03 02:58:40 +0900

    update ModalTest.js

--- screens/ModalTest.js
+++ screens/ModalTest.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Block, Button } from "galio-framework";
 import { ChatModal } from "../components/molecules/ChatModal";
 import {ProfileModal} from "../components/molecules/ProfileModal"
+import { MyPageModal } from "../components/molecules/MyPageModal";
 
 
 const ModalTest = (props) => {
@@ -21,7 +22,9 @@ const ModalTest = (props) => {
       </Button>
       </Block>
 
-      <ProfileModal isOpen={isOpen} setIsOpen={setIsOpen} />
+      {/* <ProfileModal isOpen={isOpen} setIsOpen={setIsOpen} /> */}
+      {/* <ChatModal isOpen={isOpen} setIsOpen={setIsOpen} /> */}
+      <MyPageModal isOpen={isOpen} setIsOpen={setIsOpen} />
     </>
   );
 }
