0855bcf8b6dbf143b910f7a9329a44e347e68365
kensan914 <52157596+kensan914@users.noreply.github.com>
2021-02-15 20:16:49 +0900

    fix AppDelegate.h

--- ios/fullfii/AppDelegate.h
+++ ios/fullfii/AppDelegate.h
@@ -12,8 +12,15 @@
 
 #import <EXUpdates/EXUpdatesAppController.h>
 
+/* push notification https://qiita.com/iwashi1t/items/517cda73dba715025b6c */
+#import <UserNotifications/UNUserNotificationCenter.h>
+/* push notification */
+
 @interface AppDelegate : UMAppDelegateWrapper <RCTBridgeDelegate, EXUpdatesAppControllerDelegate>
+
+/* push notification https://qiita.com/iwashi1t/items/517cda73dba715025b6c */
 @interface AppDelegate : UIResponder <UIApplicationDelegate, RCTBridgeDelegate, UNUserNotificationCenterDelegate>
+/* push notification */
 
 @property(nonatomic, strong) UMModuleRegistryAdapter *moduleRegistryAdapter;
 @property(nonatomic, strong) UIWindow *window;
